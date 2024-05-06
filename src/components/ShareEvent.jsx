4e6845a8fa98b441e1165811b60c2ab9f69568a7
SergiDescayre <lepetitdesca@gmail.com>
2024-05-06 17:52:23 +0200

    arreglo error vercel

--- src/components/ShareEvent.jsx
+++ src/components/ShareEvent.jsx
@@ -1,5 +1,5 @@
 import facebook from "../assets/facebook.svg";
-import whatsApp from "../assets/whatsApp.svg";
+//import whatsApp from "../assets/whatsApp.svg";
 
 const ShareEvent = () => {
   const eventUrl = window.location.href;
@@ -19,15 +19,15 @@ const ShareEvent = () => {
     window.open(facebookUrl, "_blank");
   };
 
-  const shareOnWhatsApp = () => {
-    const whatsAppUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(eventTitle)}%20-%20${encodeURIComponent(eventUrl)}`;
-    window.open(whatsAppUrl, "_blank");
-  };
+  // const shareOnWhatsApp = () => {
+  //   const whatsAppUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(eventTitle)}%20-%20${encodeURIComponent(eventUrl)}`;
+  //   window.open(whatsAppUrl, "_blank");
+  // };
 
   return (
     <div className="flex gap-4 flex-wrap">
       <img onClick={shareOnFacebook} src={facebook} alt="facebook" />
-      <img onClick={shareOnWhatsApp} src={whatsApp} alt="whatsApp" />
+      {/* <img onClick={shareOnWhatsApp} src={whatsApp} alt="whatsApp" /> */}
       <button className="btn" onClick={compartir}>
         Compartir
       </button>
