a875794ce79f1789329fda09d6c513a2b27db254
SergiDescayre <lepetitdesca@gmail.com>
2024-05-07 07:59:59 +0200

    millora de icones

--- src/components/ShareEvent.jsx
+++ src/components/ShareEvent.jsx
@@ -12,21 +12,36 @@ const ShareEvent = ({ fest }) => {
     window.open(facebookUrl, "_blank");
   };
 
+  // const shareOnWhatsApp = () => {
+  //   const whatsAppUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(eventTitle)}%20-%20${encodeURIComponent(eventUrl)}`;
+  //   window.open(whatsAppUrl, "_blank");
+  // };
+
   const shareOnWhatsApp = () => {
-    const whatsAppUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(eventTitle)}%20-%20${encodeURIComponent(eventUrl)}`;
-    window.open(whatsAppUrl, "_blank");
+    // URL de la imagen por defecto
+
+    // URL de tu sitio web o recurso que deseas compartir
+
+    // Mensaje que se mostrará en WhatsApp
+    const text = encodeURIComponent("¡Echa un vistazo a esto!");
+
+    // Construyendo el enlace con metadatos enriquecidos
+    const whatsappUrl = `https://api.whatsapp.com/send?text=${text}&amp;url=${eventUrl}&amp;image=${encodeURIComponent(eventImage)}`;
+
+    // Abriendo el enlace en una nueva ventana
+    window.open(whatsappUrl, "_blank");
   };
 
   return (
     <div className="flex gap-2">
       <img
-        className="w-5 md:w-9 cursor-pointer"
+        className="w-5 h-5 md:w-9 md:h-9 cursor-pointer"
         onClick={shareOnFacebook}
         src={facebook}
         alt="facebook"
       />
       <img
-        className="w-5 md:w-9 cursor-pointer"
+        className="w-5 h-5 md:w-9 md:h-9 cursor-pointer"
         onClick={shareOnWhatsApp}
         src={whatsApp}
         alt="whatsApp"
--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,26 +1,7 @@
 import React from "react";
 
-import ShareEvent from "../components/ShareEvent";
-import Editor from "../components/Editor";
-import { useLocation } from "react-router-dom";
-
 const Calendar = () => {
-  const location = window.location.href;
-  console.log(location);
-  return (
-    <div>
-      Calendar
-      <ShareEvent
-        eventTitle={"hola"}
-        eventUrl={
-          "https://design2tailwind.com/blog/tailwindcss-hide-scrollbar/"
-        }
-      />
-      <Editor />
-      <h1>hola</h1>
-      <p>adios</p>
-    </div>
-  );
+  return <div>calendar</div>;
 };
 
 export default Calendar;
