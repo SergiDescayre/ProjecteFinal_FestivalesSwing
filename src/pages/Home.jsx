d5f2b9d86b9616b8e8edfb8ed7bf47c7e5797d49
Sergi Descayre <lepetitdesca@gmail.com>
2024-05-21 13:10:46 +0200

    wave

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,8 @@ const Home = () => {
   const {t} = useTranslation("global")
   return (
     <>
-      <div className="bg-[url('./assets/Lindy_Hop.jpeg')] bg-cover bg-no-repeat ">
+      <div className="relative bg-[url('./assets/Lindy_Hop.jpeg')] bg-cover bg-no-repeat ">
+      <div className="-z-0 clip absolute bottom-[-2px] min-w-full h-12 md:h-28 bg-zinc-800"></div>
         <div className="bg-zinc-950 md:min-h-[470px] bg-opacity-80">
           <div className=" flex flex-col md:py-[60px]">
             <span className="leading-[50px] text-4xl text-orange-200 m-9 md:text-center md:text-5xl">
@@ -16,7 +17,10 @@ const Home = () => {
             </span>
             {/* <span className="text-2xl ml-9 text-stone-100 mb-9 md:text-center md:text-3xl">Festivales que despiertan tu pasión. </span> */}
           </div>
+          <div className="z-0">
           <FormSearchHome />
+
+          </div>
         </div>
       </div>
 
